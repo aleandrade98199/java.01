fab3e98d13594e5fd963fb11d99e55add93388a3
Alexandra <alexandraandrade636@gmail.com>
2019-08-19 20:59:55 -0300

    aprendendo a usar variaveis

---
+++ TestaVariaveis.java/src/comandos.java
@@ -0,0 +1,13 @@
+
+public class comandos {
+
+	public static void main(String[] args) {
+        String nome= "Luana";
+        System.out.println(nome);
+        
+        int idade = 30;
+        System.out.println(idade);
+        
+        
+	}   
+}
